67b22f241757863ce8589f3232fd3ffdaea3d8c9
birincioglu1 <30694561+birincioglu1@users.noreply.github.com>
2019-11-27 16:25:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/havadurumu/MySingleton.kt
@@ -0,0 +1,59 @@
+package com.example.havadurumu
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.LruCache
+
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.ImageLoader
+import com.android.volley.toolbox.Volley
+
+class MySingleton private constructor(context: Context) {
+    private var requestQueue: RequestQueue? = null
+    val imageLoader: ImageLoader
+
+    init {
+        ctx = context
+        requestQueue = getRequestQueue()
+
+        imageLoader = ImageLoader(requestQueue,
+            object : ImageLoader.ImageCache {
+                private val cache = LruCache<String, Bitmap>(20)
+
+                override fun getBitmap(url: String): Bitmap {
+                    return cache.get(url)
+                }
+
+                override fun putBitmap(url: String, bitmap: Bitmap) {
+                    cache.put(url, bitmap)
+                }
+            })
+    }
+
+    fun getRequestQueue(): RequestQueue ?{
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx?.applicationContext)
+        }
+        return requestQueue
+    }
+
+    fun <T> addToRequestQueue(req: Request<T>) {
+        getRequestQueue()?.add(req)
+    }
+
+    companion object {
+        private var instance: MySingleton? = null
+        private var ctx: Context?=null
+
+        @Synchronized
+        fun getInstance(context: Context?): MySingleton? {
+            if (instance == null) {
+                instance = MySingleton(context!!)
+            }
+            return instance
+        }
+    }
+}
